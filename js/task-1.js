1eab9697125a60349d67857a69110677f47fefef
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-04-18 11:59:05 +0300

    task-1 is done

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,29 @@
-function slugify(title) {
-    const lowerslug = title.toLowerCase();
-    const slug = lowerslug.split(" ");
-    return slug.join("-");
+function isEnoughCapacity(products, containerSize) {
+    let sum = 0;
+    let productsArray = Object.values(products);
+    for (let i = 0; i < productsArray.length; i++) {
+        sum += productsArray[i];       
+    } 
+
+    if (sum <= containerSize) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-slugify("Arrays for beginners");
-slugify("English for developer");
-slugify("Ten secrets of JavaScript");
-slugify("How to become a JUNIOR developer in TWO WEEKS");
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
 
-console.log(slugify("Arrays for beginners")); 
-console.log(slugify("English for developer")); 
-console.log(slugify("Ten secrets of JavaScript")); 
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
