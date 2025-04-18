69b3d98a6fa1fc6f049b79162e09051de7d46dd0
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-04-18 19:08:58 +0300

    task-3 is done

--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,25 @@
-function filterArray(numbers, value) {
-    const newArray = [];
-    const arrayLength = numbers.length;
-
-    for (let i = 0; i < arrayLength; i++) {
-        if (numbers[i] > value) {
-            newArray.push(numbers[i]);
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        if (this.username !== newName) {
+            this.username = newName;
         }
-    }
-    return newArray;
-}
+    },
+    updatePlayTime(hours) {
+        if (this.playTime !== hours) {
+            this.playTime += hours;
+        }
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
 
-filterArray([1, 2, 3, 4, 5], 3);
-filterArray([1, 2, 3, 4, 5], 4);
-filterArray([1, 2, 3, 4, 5], 5);
-filterArray([12, 24, 8, 41, 76], 38);
-filterArray([12, 24, 8, 41, 76], 20);
+console.log(profile.getInfo()); 
+profile.changeUsername("Marco");
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5)); 
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
